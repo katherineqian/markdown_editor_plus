2f933712e428fb8996259e9c1490762269d0f7a1
katherineqian <21981137+katherineqian@users.noreply.github.com>
2026-06-04 17:03:45 -0500

    experiment: align text top

--- lib/widgets/markdown_auto_preview.dart
+++ lib/widgets/markdown_auto_preview.dart
@@ -311,46 +311,42 @@ class _MarkdownAutoPreviewState extends State<MarkdownAutoPreview> {
             children: [
               // show toolbar
               if (!widget.readOnly)
-                Container(
-                  color: Colors.yellow,
-                  child: MarkdownToolbar(
-                    markdownSyntax: widget.markdownSyntax,
-                    // key: const ValueKey<String>("zmarkdowntoolbar"),
-                    controller: _internalController,
-                    autoCloseAfterSelectEmoji: widget.autoCloseAfterSelectEmoji,
-                    toolbar: _toolbar,
-                    onActionCompleted: () {
-                      widget.onChanged?.call(_internalController.text);
-                    },
-                    previewed: _previewed,
-                    onPreviewChanged: () {
-                      if (widget.writeOnly) {
-                        setState(() {
-                          _previewed = !_previewed;
-                        });
-                      } else {
-                        // Remove focus first
-                        _internalFocus.unfocus();
-
-                        // Then remove widget from widget tree
-                        setState(() {
-                          _focused = !_focused;
-                        });
-                      }
-                    },
-                    unfocus: () {
+                MarkdownToolbar(
+                  markdownSyntax: widget.markdownSyntax,
+                  // key: const ValueKey<String>("zmarkdowntoolbar"),
+                  controller: _internalController,
+                  autoCloseAfterSelectEmoji: widget.autoCloseAfterSelectEmoji,
+                  toolbar: _toolbar,
+                  onActionCompleted: () {
+                    widget.onChanged?.call(_internalController.text);
+                  },
+                  previewed: _previewed,
+                  onPreviewChanged: () {
+                    if (widget.writeOnly) {
+                      setState(() {
+                        _previewed = !_previewed;
+                      });
+                    } else {
+                      // Remove focus first
                       _internalFocus.unfocus();
-                    },
-                    showEmojiSelection: widget.showEmojiSelection,
-                    emojiConvert: widget.emojiConvert,
-                    toolbarBackground: widget.toolbarBackground,
-                    expandableBackground: widget.expandableBackground,
-                    borderColor: widget.borderColor,
-                  ),
+
+                      // Then remove widget from widget tree
+                      setState(() {
+                        _focused = !_focused;
+                      });
+                    }
+                  },
+                  unfocus: () {
+                    _internalFocus.unfocus();
+                  },
+                  showEmojiSelection: widget.showEmojiSelection,
+                  emojiConvert: widget.emojiConvert,
+                  toolbarBackground: widget.toolbarBackground,
+                  expandableBackground: widget.expandableBackground,
+                  borderColor: widget.borderColor,
                 ),
               Container(
                 margin: const EdgeInsets.only(top: 10),
-                color: Colors.purple,
                 child: _previewed
                     ? Align(
                         alignment: Alignment.centerLeft,
@@ -378,6 +374,7 @@ class _MarkdownAutoPreviewState extends State<MarkdownAutoPreview> {
       inputFormatters: [
         if (widget.emojiConvert) EmojiInputFormatter(),
       ],
+      textAlignVertical: TextAlignVertical.top,
       onChanged: widget.onChanged,
       onTap: widget.onTap,
       readOnly: widget.readOnly,
